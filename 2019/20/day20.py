f76aa8457eb44d7d4c99d6c2d1f037ef770b539d
Alexander Ludwig <alexander.la.ludwig@bmw.de>
2019-12-20 13:18:45 +0100

    day 20 in progress

--- 2019/20/day20.py
+++ 2019/20/day20.py
@@ -12,26 +12,6 @@ DIRECTIONS = [UP, RIGHT, DOWN, LEFT]
 PASSAGE = '.'
 WALL = '#'
 
-class Portal:
-
-    def __init__(self, pos1, pos2):
-        self.pos1 = pos1
-        self.pos2 = pos2
-
-
-class Dijkstra:
-
-    def __init__(self, nodes):
-        self.distance = {}
-        self.pred = {}
-        self.done = {}
-        for node in nodes:
-            self.distance[node] = math.inf
-            self.pred[node] = None
-
-    def shortest(self, start, end):
-        pass
-
 def getNeighbors(position):
     adjacents = []
     for direction in DIRECTIONS:
@@ -51,7 +31,6 @@ def getLetterTile(maze, neighbors):
         if tile in UPPER:
             return neighbor
     return None
-
 
 def getMaze(input):
     maze = {}
@@ -83,11 +62,90 @@ def getPortals(maze):
             portalTiles.append(passageTile)
             portals[portalName] = portalTiles
     return portals
+
+def getNextNodes(maze, position, nodes, portals):
+    mazeCopy = maze.copy()
+    nextNodes = set()
+    distance = 0
+    starts = set([position])
+
+    while len(starts) > 0:
+        distance += 1
+        starts = nextStep(mazeCopy, starts, portals)
+        tmpStarts = set()
+        for start in starts:
+            if start in nodes:
+                nextNodes.add((start, distance))
+                tmpStarts.add(start)
+        starts = starts - tmpStarts
+    return nextNodes
+
+def nextStep(maze, starts, portals):
+    newStarts = set([])
+    for start in starts:
+        adjacents = getNeighbors(start)
+        if start in portals:
+            adjacents.append(portals[start])
+        maze.pop(start, None)
+        for adjacent in adjacents:
+            if adjacent in maze and maze[adjacent] == PASSAGE:
+                newStarts.add(adjacent)
+    return newStarts
+
+def djikstra(nodes, maze, portals, start, end):
+    print(nodes)
+    distance = {}
+    pred = {}
+    done = {}
+    for node in nodes:
+        distance[node] = math.inf
+        pred[node] = None
     
+    
+    done[start] = 0
+    
+    currentNode = start
+
+    nextNodes = getNextNodes(maze, currentNode, nodes, portals)
+    for nextNode in nextNodes:
+        distance[nextNode[0]] = nextNode[1]
+        pred[nextNode[0]] = nextNode[1]
+
+    while len(distance.keys()) > 0:
+        print(distance)
+        nearest = min(distance, key=distance.get)
+        print(currentNode, nearest)
+        print("----------------------")
+        done[nearest] = distance.pop(nearest)
+        currentNode = nearest
+
+        nextNodes = getNextNodes(maze, currentNode, nodes, portals)
+        baseDistance = done[nearest]
+        for nodeItem in nextNodes:
+            newDistance = baseDistance + nodeItem[1]
+            currentDistance = distance[nextNode[0]]
+            if newDistance < currentDistance:
+                distance[nodeItem[0]] = newDistance
+                pred[nodeItem[0]] = currentNode
+
+
+
 # Open input files and get intcodeprogram
 script_dir = os.path.dirname(__file__)
 inputFile = open(script_dir + "/input", "r")
 
 maze = getMaze(inputFile)
-getPortals(maze)
+portals = getPortals(maze)
+
+tmpPortals = {}
+for portal in portals.values():
+    if len(portal) > 1:
+        tmpPortals[portal[0]] = portal[1]
+        tmpPortals[portal[1]] = portal[0]
+
+allPortalNodes = []
+for portalNodes in portals.values():
+    allPortalNodes = allPortalNodes + portalNodes
+
+djikstra(allPortalNodes, maze, tmpPortals, portals['AA'][0], portals['ZZ'][0])
 
