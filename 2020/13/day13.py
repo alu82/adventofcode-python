9b8223f81979d921bb2d350ed7c1835b7b2f3dd8
alu82 <ldw_srv@posteo.de>
2020-12-14 07:27:28 +0100

    day 13 2020 refactorings

--- 2020/13/day13.py
+++ 2020/13/day13.py
@@ -8,37 +8,30 @@ def solve_1(input):
     print(result[1]*result[0])
 
 def solve_2(input):
-    bus_ids = [b for b in input[1].split(",")]
+    bus_ids = input[1].split(",")
+    solution = 0
+    steps = 1
     
     for idx, bus_id in enumerate(bus_ids):
         if bus_id == "x":
             continue
-        if idx == 0:
-            solution = 0
-            steps = int(bus_id)
+        
+        bus = int(bus_id)
+        while solution <= bus*steps:
+            if (solution+idx)%bus == 0:
+                steps *= bus # this solution works for primes. for a general solution: take the lowest common multiple of steps and bus?
+                break
+            else:
+                solution += steps
         else:
-            bus = int(bus_id)
-            while True:
-                if (solution+idx)%bus == 0:
-                    steps *= bus
-                    break
-                else:
-                    solution += steps             
+            solution = -1 #no solution
+
     print(solution)
-
-
-def find(ref, bus, idx):
-    mult = 1
-    while True:
-        if (ref*mult+idx)%bus == 0:
-            return mult
-        mult += 1
-
 
 script_dir = os.path.dirname(__file__)
 with open(script_dir + "/input", "r") as myInput:
     start_time = time.time()
     input = [line.strip() for line in myInput]
-    # solve_1(input)
+    solve_1(input)
     solve_2(input)
     print("--- Execution time %s s ---" % (time.time() - start_time))
