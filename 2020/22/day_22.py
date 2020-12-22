3ed40908c5a5b591358610ec899e4d73ae202c15
alu82 <ldw_srv@posteo.de>
2020-12-22 22:17:23 +0100

    day 22 2020 refactorings

--- 2020/22/day_22.py
+++ 2020/22/day_22.py
@@ -4,33 +4,17 @@ import time
 def solve_1(input):
     deck_1 = [int(p) for p in input[0] if not p.startswith("Player")]
     deck_2 = [int(p) for p in input[1] if not p.startswith("Player")]
-
-    while len(deck_1) > 0 and len(deck_2) > 0:
-        card_1 = deck_1.pop(0)
-        card_2 = deck_2.pop(0)
-
-        if card_1 > card_2:
-            deck_1.append(card_1)
-            deck_1.append(card_2)
-        else:
-            deck_2.append(card_2)
-            deck_2.append(card_1)
-
-    winner = deck_1 if len(deck_1) > 0 else deck_2
-    return winning_score(winner)
+    return max([winning_score(deck) for deck in combat(deck_1, deck_2, False)])
 
 def solve_2(input):
     deck_1 = [int(p) for p in input[0] if not p.startswith("Player")]
     deck_2 = [int(p) for p in input[1] if not p.startswith("Player")]
-    return max([winning_score(deck) for deck in combat(deck_1, deck_2)])
+    return max([winning_score(deck) for deck in combat(deck_1, deck_2, True)])
 
 def winning_score(deck):
-    solution = 0
-    for idx, value in enumerate(deck[::-1]):
-        solution += (value * (idx+1))
-    return solution
+    return sum([value*(idx+1) for idx,value in enumerate(deck[::-1])])
 
-def combat(deck_1, deck_2):
+def combat(deck_1, deck_2, recursive):
     rounds = list()
     while len(deck_1) > 0 and len(deck_2) > 0:
         round = (deck_1.copy(), deck_2.copy())
@@ -41,7 +25,7 @@ def combat(deck_1, deck_2):
 
         card_1 = deck_1.pop(0)
         card_2 = deck_2.pop(0)
-        if len(deck_1) < card_1 or len(deck_2) < card_2:
+        if not recursive or (len(deck_1) < card_1 or len(deck_2) < card_2):
             if card_1 > card_2:
                 deck_1.append(card_1)
                 deck_1.append(card_2)
@@ -52,7 +36,7 @@ def combat(deck_1, deck_2):
             if len(deck_1) == 0 or len(deck_2) == 0:
                 return deck_1, deck_2
         else:
-            sub_deck_1, sub_deck_2 = combat(deck_1[:card_1], deck_2[:card_2])
+            sub_deck_1, sub_deck_2 = combat(deck_1[:card_1], deck_2[:card_2], recursive)
             if len(sub_deck_1) > 0 or (len(sub_deck_1) > 0 and len(sub_deck_2) > 0):
                 deck_1.append(card_1)
                 deck_1.append(card_2)
